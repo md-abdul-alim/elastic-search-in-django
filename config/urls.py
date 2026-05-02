c12c65b84626466cfd4b26c01d6bf76167a8720e
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-05-02 11:04:41 +0600

    Add search_product view and update URL configuration

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import search_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', search_product, name='search_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def search_product(request):
+
+    data = {
+        'status': 'success',
+        'message': 'Product search successful',
+        'products': [
+            {
+                'id': 1,
+                'name': 'Product 1',
+                'price': 10.99
+            },
+            {
+                'id': 2,
+                'name': 'Product 2',
+                'price': 19.99
+            }
+        ]
+    }
+    return JsonResponse(data)
